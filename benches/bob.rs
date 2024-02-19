3f6b7c65a4c6308947d619e6434d799fa0212e77
atamakahere <git@atamaka.maskmy.id>
2024-02-19 16:09:25 +0530

    style: distinct naming

--- benches/bob.rs
+++ benches/bob.rs
@@ -7,49 +7,49 @@ use bob::{
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn randmutrefowned(c: &mut Criterion) {
-    c.bench_function("mutrefowned", |b| {
+    c.bench_function("randmutrefowned", |b| {
         b.iter(CatMutRefBuilderOwnedTypes::random_build)
     });
 }
 
 fn randmutrefbrw(c: &mut Criterion) {
-    c.bench_function("mutrefbrw", |b| {
+    c.bench_function("randmutrefbrw", |b| {
         b.iter(CatMutRefBuilderBorrowTypes::random_build)
     });
 }
 
 fn randownedowned(c: &mut Criterion) {
-    c.bench_function("ownedowned", |b| {
+    c.bench_function("randownedowned", |b| {
         b.iter(CatOwnedBuilderOwnedTypes::random_build)
     });
 }
 
 fn randownedbrw(c: &mut Criterion) {
-    c.bench_function("ownedbrw", |b| {
+    c.bench_function("randownedbrw", |b| {
         b.iter(CatOwnedBuilderBorrowTypes::random_build)
     });
 }
 
 fn defmutrefowned(c: &mut Criterion) {
-    c.bench_function("mutrefowned", |b| {
+    c.bench_function("defmutrefowned", |b| {
         b.iter(CatMutRefBuilderOwnedTypes::default_build)
     });
 }
 
 fn defmutrefbrw(c: &mut Criterion) {
-    c.bench_function("mutrefbrw", |b| {
+    c.bench_function("defmutrefbrw", |b| {
         b.iter(CatMutRefBuilderBorrowTypes::default_build)
     });
 }
 
 fn defownedowned(c: &mut Criterion) {
-    c.bench_function("ownedowned", |b| {
+    c.bench_function("defownedowned", |b| {
         b.iter(CatOwnedBuilderOwnedTypes::default_build)
     });
 }
 
 fn defownedbrw(c: &mut Criterion) {
-    c.bench_function("ownedbrw", |b| {
+    c.bench_function("defownedbrw", |b| {
         b.iter(CatOwnedBuilderBorrowTypes::default_build)
     });
 }
